459e295e6854139acc1640bc477312fb7d622852
Daniel Stiner <danstiner@gmail.com>
2015-09-23 00:50:56 -0700

    Properly set up exe and tests to depend on library project

---
+++ test/self-formatting/Spec.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Language.Haskell.Format.TestHelpers as Hfmt
+
+import Test.Framework                      (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.HUnit                          hiding (Test)
+
+main :: IO ()
+main = defaultMain [
+    testGroup "Check source formatting" (hUnitTestToTests $ Hfmt.hunitPackage "hfmt.cabal")
+  ]
