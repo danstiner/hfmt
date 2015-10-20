d2e4201c9986bfab76b45797bda9d093dcc08add
Daniel Stiner <danstiner@gmail.com>
2015-10-19 23:08:26 -0700

    Initial empty set of pure tests

---
+++ test/pure/Spec.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Test.Framework                 (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.HUnit                     hiding (Test)
+
+main :: IO ()
+main = defaultMain []
