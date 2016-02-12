aab4de22f5f2fa1276b8161b9faf7a4f34ffa979
Daniel Stiner <danstiner@gmail.com>
2016-02-11 22:46:16 -0800

    Qualify HLint3 imports to avoid ambiguities

--- src/Language/Haskell/Format/HLint.hs
+++ src/Language/Haskell/Format/HLint.hs
@@ -5,25 +5,25 @@ import Language.Haskell.Format.Internal
 
 import Control.Applicative
 import Language.Haskell.Exts.Annotated     as Hse
-import Language.Haskell.HLint3             hiding (autoSettings)
+import qualified Language.Haskell.HLint3 as HLint3
 
 formatter = undefined
 
-suggester :: (ParseMode, [Classify], Hint) -> Formatter
+suggester :: (ParseMode, [HLint3.Classify], HLint3.Hint) -> Formatter
 suggester = mkSuggester . hlint
 
-hlint :: (ParseMode, [Classify], Hint) -> HaskellSource -> Either String [Suggestion]
+hlint :: (ParseMode, [HLint3.Classify], HLint3.Hint) -> HaskellSource -> Either String [Suggestion]
 hlint (parseMode, classifications, hint) (HaskellSource source) =
   getSuggestions <$> parseResultAsEither (parse source)
   where
     parse = Hse.parseFileContentsWithComments parseMode
-    getSuggestions moduleSource = map ideaToSuggestion $ applyHints classifications hint
+    getSuggestions moduleSource = map ideaToSuggestion $ HLint3.applyHints classifications hint
                                                            [moduleSource]
 
-autoSettings :: IO (ParseMode, [Classify], Hint)
+autoSettings :: IO (ParseMode, [HLint3.Classify], HLint3.Hint)
 autoSettings = do
-  (fixities, classify, hints) <- findSettings (readSettingsFile Nothing) Nothing
-  return (hseFlags (parseFlagsAddFixities fixities defaultParseFlags), classify, resolveHints hints)
+  (fixities, classify, hints) <- HLint3.findSettings (HLint3.readSettingsFile Nothing) Nothing
+  return (HLint3.hseFlags (HLint3.parseFlagsAddFixities fixities HLint3.defaultParseFlags), classify, HLint3.resolveHints hints)
 
 parseResultAsEither :: ParseResult a -> Either String a
 parseResultAsEither (ParseOk a) = Right a
@@ -34,5 +34,5 @@ parseResultAsEither (ParseFailed loc error) =
     filename = "[" ++ srcFilename loc ++ "]"
     linecol = "(" ++ show (srcLine loc) ++ ":" ++ show (srcColumn loc) ++ ")"
 
-ideaToSuggestion :: Idea -> Suggestion
+ideaToSuggestion :: HLint3.Idea -> Suggestion
 ideaToSuggestion = Suggestion . show
