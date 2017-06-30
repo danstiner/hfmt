515988a9ef32964abb0889a2c4b170f964144f6c
Ryan Spencer <rspencer@freelancer.com>
2017-06-30 10:30:56 +1000

    Deduplicate some WriteSources logic in PrintSources

--- app/Actions.hs
+++ app/Actions.hs
@@ -21,7 +21,7 @@ act options r@(Reformat input source result) = act' (optAction options)
     act' PrintDiffs =
       when wasReformatted (printDiff input source result) >> return r
     act' PrintSources = do
-      when wasReformatted (writeSource InputFromStdIn (reformattedSource result))
+      when wasReformatted (printSource $ reformattedSource result)
       return (Reformat input (reformattedSource result) result)
     act' PrintFilePaths = when wasReformatted (print input) >> return r
     act' WriteSources = do
@@ -49,6 +49,9 @@ showDiff (HaskellSource a) (HaskellSource b) = render (toDoc diff)
     toDoc = prettyContextDiff (text "Original") (text "Reformatted") text
     diff = getContextDiff linesOfContext (lines a) (lines b)
     linesOfContext = 1
+
+printSource :: HaskellSource -> IO ()
+printSource (HaskellSource source) = putStr source
 
 writeSource :: InputFile -> HaskellSource -> IO ()
 writeSource (InputFilePath path) (HaskellSource source) = writeFile path source
